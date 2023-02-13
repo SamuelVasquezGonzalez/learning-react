cbb2e4608552de06bb2149fbf9b3ba44a312e158
SamuelVasquezGonzalez <samuelgonza2006@gmail.com>
2023-02-12 19:02:35 -0500

    primera clase finalizada, 12 de febrero

---
+++ operadorternario.js
@@ -0,0 +1,13 @@
+// Los operadores ternarios son una clase de condicionales que consa de tres partes
+// veaamos un ejemplp
+
+let patas = 4;
+let accion = "ladrar";
+
+// le estamos preguntando que si patas es igual a 4 y accion es igual a ladrar, nos devuelva "ES UN PERRO", si la condicion no se cumple: nos devolvera "NO ES UN PERRO"
+// Veamolos de esta forma
+// ? es equivalente a "if"
+// : es equivalente a "else"
+
+                                             //TRUE                         //FALSE
+(patas == 4) && (accion == "ladrar") ? console.log("es un perro") : console.log("no es un perro")
